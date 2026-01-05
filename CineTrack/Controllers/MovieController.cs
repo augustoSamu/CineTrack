c611eaa369b5cd6971a20be191475349bf641e33
augustoSamu <samuca.augusto05@gmail.com>
2026-01-04 23:36:15 -0300

    feat: adjust initial scaffold

---
+++ CineTrack/Controllers/MovieController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CineTrack.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController
+    {
+        [HttpGet]
+        public string GetMovies()
+        {
+            return "List of movies";
+        }
+
+        [HttpPost]
+        public string PostMovies()
+        {
+            return "Add a movie";
+        }
+
+        [HttpPut("{id:int}/done")]
+        public string PostMovies(int id)
+        {
+            return "Movie x updated to watched";
+        }
+    }
+}
---
+++ CineTrack/Entities/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace CineTrack.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Watched { get; set; }
+        public DateTime WatchedAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
